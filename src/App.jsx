e8ed640071cde05071f729517bce1d3fdd33761a
Sturgart <wesleyicaro4@gmail.com>
2023-03-10 00:15:20 -0300

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,49 @@ import { useState } from 'react';
 // todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
+  const [_email_, setEmail] = useState('')
+  const [_password_, setPassword] = useState('')
+
+  const handleEmail = (event) => {
+    const value = event.target.value;
+    console.log(value)
+  }
+
+
+
+  // const acess = async () => {
+  //   const next = {
+  //     email: _email_,
+  //     password: _password_
+  //   }
+
+  //   login(next.email, next.password)
+
+  //   console.log(next.email)
+  //   console.log(next.password)
+
+  //   setEmail('pato')
+  //   setPassword('')
+  // }
+
+
   return (
     <div className='wrapper'>
-      <div className='login-form'>
+      <div className='login-form' type='form'>
         <h1>Login Form 🐞</h1>
         {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
         <div className='errorMessage'></div>
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
-          <input id={'email'} type={'email'} autoComplete='off' />
+          <input name='emailzada' id={'email'} type={'email'} autoComplete='off' value={_email_ || ''} onChange={handleEmail} />
         </div>
         <div className='row'>
           <label htmlFor={'password'}>Password</label>
-          <input id={'password'} type={'password'} />
+          <input name='senhazada' id={'password'} type={'password'} value={_password_ || ''} onChange={e => setPassword(e.target.value)}/>
         </div>
 
         <div className='button'>
-          <button>Login</button>
+          {/* <button onClick={acess ? _password_.length >= 6 && _email_.length >= 1 : }>Login</button> */}
         </div>
       </div>
     </div>

110fbe345b9ae3f6f8a5c639645a7f9278212bcf
Sturgart <wesleyicaro4@gmail.com>
2023-03-12 23:56:35 -0300

    Main commit

--- challenger/src/App.jsx
+++ challenger/src/App.jsx
@@ -8,19 +8,27 @@ import { useState } from 'react';
 // * Não é permitido usar refs
 //
 // Tarefas:
-// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
-// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
-// todo - Desabilite o botão de Login equanto você está executando o login.
-// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
-// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+// O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
+// Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// Desabilite o botão de Login equanto você está executando o login.
+// Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
+// Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
   const [_email_, setEmail] = useState('')
   const [_password_, setPassword] = useState('')
+  const [error, setError] = useState(null)
+  const [isRequesting, setIsRequesting] = useState(false)
+  const [sucess, setSucess] = useState(null)
 
   const handleEmail = (event) => {
-    const value = event.target.value;
-    console.log(value)
+    const {value} = event.target;
+    setEmail(value)
+  }
+
+  const handlePassword = (event) => {
+    const {value} = event.target;
+    setPassword(value)
   }
 
 
@@ -31,12 +39,36 @@ export default function LoginForm() {
   //     password: _password_
   //   }
 
-  //   login(next.email, next.password)
+  const handleSubmit =  () => {
+    console.log('submitted')
+
+    setError(null)
+    setIsRequesting(true)
+
+    let values = {
+      email: _email_,
+      password: _password_
+    }
+
+    login(values)
+      .then((sucess) => {
+        alert('Login efetuado com sucesso!')
+        console.log(sucess)
+       })
+      .catch((error) => {
+        console.log(error)
+        setError(error)
+      }).finally(() => {
+        setIsRequesting(false)
+      })
+  }
+
+  
 
   //   console.log(next.email)
   //   console.log(next.password)
 
-  //   setEmail('pato')
+  //   setEmail('')
   //   setPassword('')
   // }
 
@@ -45,19 +77,20 @@ export default function LoginForm() {
     <div className='wrapper'>
       <div className='login-form' type='form'>
         <h1>Login Form 🐞</h1>
-        {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
-        <div className='errorMessage'></div>
+        {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro.
+        */}
+        {error && <div className='errorMessage'>{error.message}</div>}
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
-          <input name='emailzada' id={'email'} type={'email'} autoComplete='off' value={_email_ || ''} onChange={handleEmail} />
+          <input name='emailzada' id={'email'} type={'email'} autoComplete='off' value={_email_} onChange={handleEmail} />
         </div>
         <div className='row'>
           <label htmlFor={'password'}>Password</label>
-          <input name='senhazada' id={'password'} type={'password'} value={_password_ || ''} onChange={e => setPassword(e.target.value)}/>
+          <input name='senhazada' id={'password'} type={'password'} value={_password_} onChange={handlePassword}/>
         </div>
 
         <div className='button'>
-          {/* <button onClick={acess ? _password_.length >= 6 && _email_.length >= 1 : }>Login</button> */}
+          <button onClick={handleSubmit} disabled={_email_ === '' || _password_.length < 6 || isRequesting} >Login</button>
         </div>
       </div>
     </div>
